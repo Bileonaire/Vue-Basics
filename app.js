b01ed846b01adb084503021b8fe64ffe0ea4812c
Leon Kioko <bileonaire@Leons-MacBook-Pro.local>
2019-07-23 09:44:58 +0300

    Add v-for loop

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+const colours = ['red', 'blue', 'green', 'indigo', 'yellow'];
+
 const book = new Vue({
-    el: '#book',
-    data: {
-      title: 'The Sirens of Titan',
-      author: 'Kurt Vonnegut',
-      summary: 'This is a summary of the Sirens of Titan.',
-      showDetail: false
-    },
-    methods: {
-        toggleDetails: function(){
-            this.showDetail = !this.showDetail;
-        }
-    }
+	el: '#book',
+	data: {
+		rainbow: colours,
+		title: 'The Sirens of Titan',
+		author: 'Kurt Vonnegut',
+		summary: 'This is a summary of the Sirens of Titan.',
+		showDetail: false,
+	},
+	methods: {
+		toggleDetails: function() {
+			this.showDetail = !this.showDetail;
+		},
+	},
 });
